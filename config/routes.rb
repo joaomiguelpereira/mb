a93b6ce0a112beec93000ad9017afaac70b324f7
jpereira <joaomiguel.pereira@gmail.com>
2011-02-05 23:15:18 +0000

    just growing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Medibooking::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +56,10 @@ Medibooking::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+    
+  root :to => "application#index"
+  get "sign_up" => "users#new"
+  
+  resources :users
+  
 end

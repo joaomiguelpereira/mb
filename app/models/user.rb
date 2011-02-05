9b316800bcce5b12dcedadfb3b5bc23d213ef38c
jpereira <joaomiguel.pereira@gmail.com>
2011-02-05 23:39:56 +0000

    addded model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   
   attr_accessible :role, :email, :email_confirmation, :firt_name, :last_name, :password, :password_confirmation
   
-  attr_accessor :password
-  attr_accessor :email_confirmation, :password_confirmation
+  attr_accessor :password, :email_confirmation, :password_confirmation
   
 end

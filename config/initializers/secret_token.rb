ba442ff354101111145f854385bfad357831adce
jpereira <jpereira@jpereira-Satellite-L635>
2011-02-05 15:13:44 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medibooking::Application.config.secret_token = 'd051e5429d7bf9723ce6389bf77dd3cf9a1a80a9d633e051a4c57a4b12c90217e4760187daa8aef2b97b5e7083a91dce3dc2ce4b3e73002b0bc463538dc159e3'

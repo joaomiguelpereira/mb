ddef5e4d6ae1c06bc5810663c258e6fd7c1984a3
jpereira <joaomiguel.pereira@gmail.com>
2011-02-07 18:53:34 +0000

    No relevance

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  def more_stylesheets(s)
+    content_for :more_stylesheets do
+      stylesheet_link_tag s 
+    end
+    
+  end
+  
+  def title(t)
+    
+    content_for :title do
+      t + " | medibooking.com"
+    end
+  end
 end

89c2111fbad043e3cbbf8c6ecec383a576a5dd1f
jpereira <joaomiguel.pereira@gmail.com>
2011-02-07 18:52:54 +0000

    No relevance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  
+  before_filter :mailer_set_url_options
+  
   layout "default"
   def index
     @title = "hello, man"
   end
   
+  
+  protected
+  def flash_error(i18nkey) 
+    flash.now[:error] = I18n.t(i18nkey) 
+  end
+  
+  private 
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
 end

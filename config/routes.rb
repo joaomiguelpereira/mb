e3d9b25ca5f1a8b32b0ecc2ab57a3573e467aa95
jpereira <joaomiguel.pereira@gmail.com>
2011-02-08 01:18:59 +0000

    no comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Medibooking::Application.routes.draw do
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
+  
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -25,13 +25,13 @@ Medibooking::Application.routes.draw do
   #       get 'sold'
   #     end
   #   end
-
+  
   # Sample resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
   #   end
-
+  
   # Sample resource route with more complex sub-resources
   #   resources :products do
   #     resources :comments
@@ -39,27 +39,32 @@ Medibooking::Application.routes.draw do
   #       get 'recent', :on => :collection
   #     end
   #   end
-
+  
   # Sample resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+  
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  
   # See how all your routes lay out with "rake routes"
-
+  
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-    
+  
   root :to => "application#index"
-  get "sign_up" => "users#new"
+  
+  
   
   resources :users
+  
+  match "user/activate/:activation_key" => "users#activate", :via=>[:get], :as=>:activate_user
+  
+  resources :sessions
   
 end

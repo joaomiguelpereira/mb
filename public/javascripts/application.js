24d66bc0d4538970254b99e4adfc0f231ce9014c
jpereira <joaomiguel.pereira@gmail.com>
2011-02-08 15:56:04 +0000

    Close flash messages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var flash_messages = {
+	
+	close : function() {
+		$('floating_message_wrapper').fade();
+	}
+}

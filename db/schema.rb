46f80afc4f20c8a6092e23c51d685971046cb887
jpereira <joaomiguel.pereira@gmail.com>
2011-02-08 01:19:35 +0000

    no comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110205230500) do
+ActiveRecord::Schema.define(:version => 20110206232805) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(:version => 20110205230500) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.boolean  "active",         :default => false
+    t.string   "activation_key"
   end
 
 end

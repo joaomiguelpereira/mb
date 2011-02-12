b8fe1b59a014a9357f4f44c7c4017fc1912678fb
jpereira <joaomiguel.pereira@gmail.com>
2011-02-12 15:29:46 +0000

    Edit profile inline

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,43 @@
 var flash_messages = {
 	
 	close : function() {
-		$('floating_message_wrapper').fade();
-	}
+		$('#floating_message_wrapper').hide();
+	}, 
+	close_future: function() {
+		
+		window.setInterval(flash_messages.close, 5000);
+	}	
 }
+
+
+var loading_indicator = {
+    show: function() {
+		$('#loading_indicator').show();
+		
+	},
+	hide: function() {
+		$('#loading_indicator').hide();
+	}	
+}
+
+$(function(){
+	
+	$("*").live( 'ajax:beforeSend', function() {
+		loading_indicator.show();
+	}),
+	
+		
+	$("*").live( 'ajax:complete', function() {
+		loading_indicator.hide();
+	}), 
+	
+	$("#floating_message_wrapper").live("float_message:loaded", function() {
+		flash_messages.close_future();
+	}),
+	element  =$("#floating_message_wrapper");
+	if (element!=null) {
+		flash_messages.close_future();
+	}
+	
+})
+

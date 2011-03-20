23bca70fd65459d7c3861451fcca171f493415cd
jpereira <joaomiguel.pereira@gmail.com>
2011-03-20 21:12:34 +0000

    addded db to deploy namespace

--- config/deploy/capistrano_database.rb
+++ config/deploy/capistrano_database.rb
@@ -2,11 +2,11 @@ require 'erb'
 
 before "deploy:setup", :db
 after "deploy:update_code", "db:symlink" 
-
-namespace :db do
-  desc "Create database yaml in shared path" 
-  task :default do
-    db_config = ERB.new <<-EOF
+namespace :deploy do
+  namespace :db do
+    desc "Create database yaml in shared path" 
+    task :default do
+      db_config = ERB.new <<-EOF
     base: &base
       adapter: mysql
       encoding: utf8
@@ -28,13 +28,14 @@ namespace :db do
       database: #{application}_production
       <<: *base
     EOF
-
-    run "mkdir -p #{shared_path}/config" 
-    put db_config.result, "#{shared_path}/config/database.yml" 
-  end
-
-  desc "Make symlink for database yaml" 
-  task :symlink do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+      
+      run "mkdir -p #{shared_path}/config" 
+      put db_config.result, "#{shared_path}/config/database.yml" 
+    end
+    
+    desc "Make symlink for database yaml" 
+    task :symlink do
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+    end
   end
 end

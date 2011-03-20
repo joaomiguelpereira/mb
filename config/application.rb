4b901450a9303690b810474063c0203fb17f5403
jpereira <joaomiguel.pereira@gmail.com>
2011-03-20 23:29:55 +0000

    filter passwords from log

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Medibooking
     config.encoding = "utf-8"
     
     # Configure sensitive parameters which will be filtered from the log file.
-    #config.filter_parameters += [:password]
+    config.filter_parameters += [:password]
   
     #Observers
     config.active_record.observers = :user_observer

18856c39e737d9265bf0e2a45a0b8ac89213397c
jpereira <joaomiguel.pereira@gmail.com>
2011-04-07 00:05:21 +0100

    Added tests for GET of specialities

--- test/functional/business_accounts_controller_test.rb
+++ test/functional/business_accounts_controller_test.rb
@@ -7,6 +7,13 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     @baccount = Factory.create(:business_account, :owner=>@badmin)
     @badmin.business_account = @baccount
     @badmin.save
+    
+    @otherbadmin = Factory.create(:business_admin)
+    @otherbaccount = Factory.create(:business_account, :owner=>@otherbadmin)
+    @otherbadmin.business_account = @otherbaccount
+    @otherbadmin.save
+    
+    
   end
   test "cannot get the dashboard if not authenticated" do
     assert_raise(WebAppException::SessionRequiredError) do
@@ -22,7 +29,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
   end
   
   test "get the dashboard for the business admin" do
-    get :index, {}, authenticated_user(@badmin)
+    get :index, {:business_account_id=>@baccount.id}, authenticated_user(@badmin)
     assert_not_nil assigns(:business_account)
     assert_equal @baccount.id, assigns(:business_account).id
   end
@@ -33,7 +40,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data = Array.new
     #create two new exceptions
     json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"1/1/2002", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_success("flash.success.business_account.availability_exceptions.update"); 
   end
   
@@ -44,7 +51,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"1/1/2002", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
     json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"1/1/2002", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
     
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_success("flash.success.business_account.availability_exceptions.update"); 
   end
   
@@ -52,7 +59,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data = Array.new
     #create two new exceptions
     json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_success("flash.success.business_account.availability_exceptions.update"); 
   end
 
@@ -60,7 +67,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data = Array.new
     #create two new exceptions
     #json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"1/1/2002", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_success("flash.success.business_account.availability_exceptions.update"); 
   end
   
@@ -68,7 +75,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data = ""
     #create two new exceptions
     #json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"1/1/2002", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_error("flash.error.business_account.availability_exceptions.update"); 
   end
   
@@ -77,7 +84,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data = Array.new
     #create two new exceptions
     json_data << {"start_date"=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"1/1/2002", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_error("flash.error.business_account.availability_exceptions.update")
   end
 
@@ -85,7 +92,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data = Array.new
     #create two new exceptions
     json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"12/12/2001", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"11/12/2001", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_error("flash.error.business_account.availability_exceptions.update") 
   end
   
@@ -93,7 +100,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
     json_data = Array.new
     #create two new exceptions
     json_data << {Availability::EXCEPTION_START_DATE_FIELD_NAME=>"232323", Availability::EXCEPTION_END_DATE_FIELD_NAME=>"11111", Availability::EXCEPTION_MOTIVE_FIELD_NAME=>"Holidays, big ones", Availability::EXCEPTION_NOTES_FIELD_NAME=>"Some notes"}
-    put :availability_exceptions, {:business_account_id=>@badmin.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
+    put :availability_exceptions, {:business_account_id=>@baccount.id, :json_data=>json_data.to_json} , authenticated_user(@badmin)
     assert_json_error("flash.error.business_account.availability_exceptions.update") 
   end
 
@@ -112,7 +119,7 @@ class BusinessAccountsControllerTest < ActionController::TestCase
       
       json_data_array << day_array
     end
-    put :availability, {:business_account_id=>@badmin.id, :json_data=>json_data_array.to_json}, authenticated_user(@badmin)
+    put :availability, {:business_account_id=>@baccount.id, :json_data=>json_data_array.to_json}, authenticated_user(@badmin)
     #json = ActiveSupport::JSON.decode(@response.body)
     #assert_equal json["message"], I18n.t("flash.success.business_account.availability.update")
     
@@ -132,14 +139,31 @@ class BusinessAccountsControllerTest < ActionController::TestCase
       json_data_array << day_array
     end
     
-    put :availability, {:business_account_id=>@badmin.id, :json_data=>json_data_array.to_json}, authenticated_user(@badmin)
+    put :availability, {:business_account_id=>@baccount.id, :json_data=>json_data_array.to_json}, authenticated_user(@badmin)
     assert_json_error("flash.error.business_account.availability.update"); 
       end
   
   test "get availability" do
-    
-    get :availability, {:business_account_id=>@badmin.id}, authenticated_user(@badmin)
+    get :availability, {:business_account_id=>@baccount.id}, authenticated_user(@badmin)
     assert_not_nil assigns(:availability)
+  end
+  
+  test "dont get specialities if not authenticated" do 
+     assert_raise(WebAppException::SessionRequiredError) do
+      get :specialities,{:business_account_id=>@baccount.id},{}
+    end
+  end
+  
+  test "dont get specialities for other user" do  
+    assert_raise(WebAppException::AuthorizationError) do
+      get :specialities,{:business_account_id=>@baccount.id},authenticated_user(@otherbadmin)
+    end
+  end
+  
+  test "get specialities" do
+    get :specialities,{:business_account_id=>@baccount.id},authenticated_user(@badmin)
+    assert_not_nil assigns(:business_account)
+    assert assigns(:specialities)
     
   end
 end

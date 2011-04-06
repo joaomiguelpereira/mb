72fcab3772cc55ce7041b86eecdf7c962475090d
Carlos Rodrigues <cmsr@lenovinho.(none)>
2011-04-07 03:23:51 +0800

    added new user

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,7 @@ owner.save!
 #Factory.create(:business,:business_admin_id=>owner.id)
 #Factory.create(:business,:business_admin_id=>owner.id)
 
-#owner = Factory.create(:business_admin, :email=>"cmsrodrigues@gmail.com", :password=>"12345")
-
+owner = Factory.create(:business_admin, :email=>"cmsrodrigues@gmail.com", :password=>"12345")
+account = Factory.create(:business_account, :owner=>owner)
+owner.business_account = account
+owner.save!
